4a01ce0379eb406c2cb4cbf1762b84400b3cdea9
Andrei Zvonkov <andrei@fosterinteractive.com>
2020-10-14 17:29:26 -0400

    nav updates

--- components/nav.js
+++ components/nav.js
@@ -5,12 +5,27 @@ const Nav = () => (
   <NavItems>
     <NavItem>
       <Link href="#one" passHref>
-        <NavLink>Section One</NavLink>
+        <NavLink>Boards</NavLink>
       </Link>
     </NavItem>
     <NavItem>
       <Link href="#two" passHref>
-        <NavLink>Section Two</NavLink>
+        <NavLink>CEOs</NavLink>
+      </Link>
+    </NavItem>
+    <NavItem>
+      <Link href="#two" passHref>
+        <NavLink>Top Talent</NavLink>
+      </Link>
+    </NavItem>
+    <NavItem>
+      <Link href="#two" passHref>
+        <NavLink>About</NavLink>
+      </Link>
+    </NavItem>
+    <NavItem>
+      <Link href="#two" passHref>
+        <NavLink>Contact</NavLink>
       </Link>
     </NavItem>
   </NavItems>
@@ -25,8 +40,13 @@ const NavItem = styled.li`
   display: inline-block;
 
   &:not(:last-child) {
-    margin-right: 2.3rem;
-    padding-right: 2.5rem;
+    margin-right: 1.8rem;
+    padding-right: 2rem;
+
+    @media (min-width: ${(props) => props.theme.screen.lg}) {
+      margin-right: 2.3rem;
+      padding-right: 2.5rem;
+    }
   }
 
   &:not(:last-child):before {
@@ -35,20 +55,19 @@ const NavItem = styled.li`
     height: 16px;
     width: 1px;
     transform: rotate(20deg);
-    background-color: var(--yellow);
+    background-color: var(--cool-grey-400);
     right: 0;
-    top: 30%;
-    color: #ccc;
+    top: 25%;
   }
 `
 
 const NavLink = styled.a`
   position: relative;
   display: inline-block;
-  font-size: 1.4rem;
+  font-size: 1.6rem;
   padding: 0 0 0.25rem;
-  color: #333;
-  opacity: 0.7;
+  color: var(--cool-grey-600);
+  font-weight: 600;
   transition: opacity 0.3s cubic-bezier(0.51, 0.92, 0.24, 1);
 
   &:after {
@@ -59,14 +78,14 @@ const NavLink = styled.a`
     right: 0;
     top: 100%;
     height: 2px;
-    background: var(--yellow);
+    background: var(--light-blue-500);
     transform: scaleX(var(--scale));
     transform-origin: var(--x) 50%;
     transition: transform 0.3s cubic-bezier(0.51, 0.92, 0.24, 1);
   }
 
   &:hover {
-    opacity: 1;
+    color: var(--light-blue-900);
   }
 
   &:hover:after {
