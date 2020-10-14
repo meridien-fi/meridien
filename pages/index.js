cfe2ea12d879050cf8624e663da69bc7b9155fe1
Andrei Zvonkov <andrei@fosterinteractive.com>
2020-10-14 17:29:44 -0400

    page head updates

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Title of the Site</title>
-        <meta name='description' content='Meta Description of the Site' />
+        <title>Meridien Consulting - Strategic Leadership Advisory</title>
+        <meta name='description' content='Personalized Insights for Boards, CEOs and Top Talent' />
       </Head>
       <Header />
       <main>
