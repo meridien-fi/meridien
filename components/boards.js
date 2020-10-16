363d3e58f33476886b8df7913aa3aaa0be882fa8
Andrei Zvonkov <andrei@fosterinteractive.com>
2020-10-16 13:59:29 -0400

    board updates and additons

--- components/boards.js
+++ components/boards.js
@@ -2,20 +2,19 @@ import styled from 'styled-components'
 
 const Boards = () => (
   <Section id="boards">
-    <Container>
+    <SectionIntro>
       <Title>Boards</Title>
 
-      <SectionIntro>
-        <SubTitle>Shaping an Effective, Cohesive Team Built for Today</SubTitle>
-        <Description>
-          A high-functioning board – with the right members united in vision –
-          positions an organization for healthy growth while maximizing value,
-          managing risk and staying resilient in times of change.
-        </Description>
-      </SectionIntro>
+      <SubTitle>Shaping an Effective, Cohesive Team Built for Today</SubTitle>
+      <Description>
+        A high-functioning board – with the right members united in vision –
+        positions an organization for healthy growth while maximizing value,
+        managing risk and staying resilient in times of change.
+      </Description>
+    </SectionIntro>
 
+    <ProcessWrap>
       <ProcessTitle>The Process</ProcessTitle>
-
       <Process>
         <ProcessItem>
           <ProcessItemTitle>Understand the Scope</ProcessItemTitle>
@@ -79,18 +78,28 @@ const Boards = () => (
           </ProcessItemList>
         </ProcessItem>
       </Process>
-      <ProcessFooter>
-        Effective boards create value. They adeptly scrutinize director
-        selection, CEO performance and succession and the board’s own dynamics.
-        Peter’s approach is purposefully built to ensure any board thoughtfully
-        addresses the issues of today and anticipates the challenges of
-        tomorrow.
-      </ProcessFooter>
-    </Container>
+      <ProcessSummary>
+        <p>
+          <strong>Effective boards</strong> create value. They adeptly
+          scrutinize director selection, CEO performance and succession and the
+          board’s own dynamics.
+        </p>
+        <p>
+          <strong>Peter’s</strong> approach is purposefully built to ensure any
+          board thoughtfully addresses the issues of today and anticipates the
+          challenges of tomorrow.
+        </p>
+      </ProcessSummary>
+    </ProcessWrap>
   </Section>
 )
 
 const Section = styled.section`
+  padding-bottom: 10rem;
+  border-bottom: 2px solid var(--grey-200);
+`
+
+const SectionIntro = styled.div`
   padding: 4rem 2rem;
 
   @media (min-width: ${(props) => props.theme.screen.md}) {
@@ -98,39 +107,21 @@ const Section = styled.section`
   }
 
   @media (min-width: ${(props) => props.theme.screen.lg}) {
-    padding: 12rem 8rem;
-  }
-`
-
-const Container = styled.div`
-  @media (min-width: ${(props) => props.theme.screen.md}) {
-    /* display: grid;
-    grid-gap: 4rem;
-    grid-template-columns: minmax(30%, 55%) 1fr; */
-  }
-`
-
-const SectionIntro = styled.div`
-  margin-bottom: 12rem;
-
-  @media (min-width: ${(props) => props.theme.screen.md}) {
-    /* display: grid;
-    grid-gap: 4rem;
-    grid-template-columns: 1fr 1fr; */
+    padding: 10rem 8rem;
   }
 `
 
 const Title = styled.h2`
   position: relative;
   padding-bottom: 2rem;
-  margin-bottom: 3rem;
-  font-size: 4rem;
+  margin-bottom: 4rem;
+  font-size: 6rem;
   line-height: 1;
   font-family: var(--serif);
   font-weight: 600;
 
   @media (min-width: ${(props) => props.theme.screen.md}) {
-    font-size: 6vw;
+    font-size: 9rem;
   }
 
   &:after {
@@ -158,81 +149,61 @@ const Description = styled.div`
 
   @media (min-width: ${(props) => props.theme.screen.md}) {
     max-width: 60%;
-    font-size: 2rem;
+    font-size: 2.4rem;
   }
 `
 
-const ProcessTitle = styled.div`
-  /* margin-bottom: 6rem; */
+const ProcessWrap = styled.div`
+  padding: 0 2rem;
+  border-bottom: 2px solid var(--grey-200);
 
   @media (min-width: ${(props) => props.theme.screen.md}) {
-    text-align: right;
+    padding: 0 4rem;
+  }
+
+  @media (min-width: ${(props) => props.theme.screen.lg}) {
+    padding: 0 8rem;
+    display: grid;
+    grid-column-gap: 6rem;
+    grid-template-columns: minmax(30%, 60%) 1fr;
+  }
+`
+
+const ProcessTitle = styled.h3`
+  margin-bottom: 4rem;
+  font-family: var(--serif);
+  font-size: 4rem;
+  font-weight: 600;
+  color: var(--grey-600);
+
+  @media (min-width: ${(props) => props.theme.screen.md}) {
+    grid-column: 1 / span 2;
+    grid-row: 1 / span 1;
+    margin-bottom: 10rem;
     font-size: 6rem;
-    font-family: var(--serif);
-    font-weight: 600;
-    color: var(--grey-400);
-
-    /* text-transform: uppercase; */
-    /* font-family: 'Playfair Display SC', 'Georgia', 'Cambria'; */
-    /* font-weight: 400; */
-  }
-`
-
-const ProcessItemTitle = styled.h4`
-  margin-bottom: 3rem;
-
-  @media (min-width: ${(props) => props.theme.screen.md}) {
-    font-size: 4rem;
-    line-height: 1;
-    font-family: var(--serif);
-    /* text-transform: uppercase; */
-    font-weight: 600;
-    color: var(--grey-600);
-
-    /* text-transform: uppercase; */
-    /* font-family: 'Playfair Display SC', 'Georgia', 'Cambria'; */
-    /* font-weight: 400; */
   }
 `
 
 const Process = styled.div`
-  margin-bottom: 12rem;
+  margin-bottom: 4rem;
 
   @media (min-width: ${(props) => props.theme.screen.md}) {
-    /* display: grid;
-    grid-template-columns: repeat(2, 1fr); */
+    grid-column: 1 / span 1;
   }
 `
 
 const ProcessItem = styled.div`
-  /* margin-bottom: 12rem; */
   position: relative;
-  padding: 10rem 6rem 3rem 0;
-  /* max-width: 60%; */
-  /* padding-left: 16rem; */
-  /* padding: 6rem 0; */
-  /* border-right: 1px solid #eee; */
-
-  /* &:before {
-    position: absolute;
-    right: 6rem;
-    top: 2rem;
-    color: #f5f5f5;
-    font-size: 12rem;
-    font-weight: 700;
-    line-height: 1;
-  } */
+  padding: 2rem 0 4rem 0;
 
   &:before {
     position: absolute;
     right: 0;
-    top: 10rem;
-    /* color: #f5f5f5; */
+    top: 0;
     font-size: 12rem;
     font-weight: 600;
     line-height: 1;
     font-family: var(--serif);
-    /* color: var(--blue-500); */
     color: var(--grey-200);
   }
 
@@ -252,34 +223,66 @@ const ProcessItem = styled.div`
     content: '4.';
   }
 
-  @media (min-width: ${(props) => props.theme.screen.md}) {
+  @media (min-width: ${(props) => props.theme.screen.lg}) {
+    padding: 2rem 0 10rem 0;
   }
 `
+
+const ProcessItemTitle = styled.h4`
+  margin-bottom: 4rem;
+  font-size: 2.4rem;
+  line-height: 1;
+  font-family: var(--serif);
+  color: var(--grey-600);
+
+  @media (min-width: ${(props) => props.theme.screen.md}) {
+    font-size: 3rem;
+  }
+`
+
 const ProcessItemList = styled.ul`
   position: relative;
   list-style-type: none;
-
-  @media (min-width: ${(props) => props.theme.screen.md}) {
-  }
 `
+
 const ProcessItemPoint = styled.li`
-  font-size: 2rem;
+  font-size: 1.8rem;
+
+  @media (min-width: ${(props) => props.theme.screen.lg}) {
+    font-size: 2rem;
+  }
 
   &:not(:last-child) {
+    margin-bottom: 1.8rem;
+  }
+`
+
+const ProcessSummary = styled.div`
+  position: relative;
+  padding: 4rem 0;
+  font-size: 2rem;
+  line-height: 1.75;
+  font-family: var(--serif);
+  color: var(--grey-700);
+
+  p {
     margin-bottom: 2rem;
   }
 
-  @media (min-width: ${(props) => props.theme.screen.md}) {
-  }
-`
-
-const ProcessFooter = styled.div`
-  padding-top: 2rem;
-  font-size: 2rem;
-  border-top: 1px solid var(--grey-200);
-
   @media (min-width: ${(props) => props.theme.screen.lg}) {
-    max-width: 70%;
+    grid-column: 2 / span 1;
+    padding: 6rem;
+    border-left: 2px solid var(--grey-200);
+
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 6rem;
+      width: 6rem;
+      height: 4px;
+      background-color: var(--blue-500);
+    }
   }
 `
 
