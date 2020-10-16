823e196467693dc696b732634cf982c88158efa1
Andrei Zvonkov <andrei@fosterinteractive.com>
2020-10-15 22:43:35 -0400

    header tweaks

--- components/header.js
+++ components/header.js
@@ -12,23 +12,27 @@ const Header = () => (
 
 const Brand = styled.div`
   margin-bottom: 2rem;
-  color: var(--grey-600);
+  color: var(--grey-700);
   font-weight: 600;
   font-size: 2rem;
 
-  @media (min-width: ${(props) => props.theme.screen.lg}) {
+  @media (min-width: ${(props) => props.theme.screen.md}) {
     margin-bottom: 0;
   }
 `
 
 const Container = styled.header`
-  padding: 2rem 4rem;
+  padding: 2rem;
   background-color: var(--beige-100);
 
-  @media (min-width: ${(props) => props.theme.screen.lg}) {
+  @media (min-width: ${(props) => props.theme.screen.md}) {
     display: flex;
-    padding: 2rem 8rem;
     align-items: center;
+    padding: 2rem 4rem;
+  }
+
+  @media (min-width: ${(props) => props.theme.screen.lg}) {
+    padding: 2rem 8rem;
   }
 `
 
