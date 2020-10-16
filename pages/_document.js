9e75ec3fc94f92cb891d9fda1786c42ba789f54d
Andrei Zvonkov <andrei@fosterinteractive.com>
2020-10-15 22:45:17 -0400

    font updates

--- pages/_document.js
+++ pages/_document.js
@@ -34,10 +34,7 @@ export default class CustomDocument extends Document {
       <Html lang='en'>
         <Head>
           <link rel='icon' href='/images/favicon.svg' />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,600;0,669;1,600&display=swap'
-            rel='stylesheet'
-          />
+          <link href='https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600&display=swap' rel='stylesheet' />
         </Head>
         <body>
           <Main />
