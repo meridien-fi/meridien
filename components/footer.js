35c0101a1cbec0900a9e20b1a2afed2b10974d33
Andrei Zvonkov <andrei@fosterinteractive.com>
2020-10-19 17:10:14 -0400

    footer updates and additions

--- components/footer.js
+++ components/footer.js
@@ -1,24 +1,55 @@
+import Link from 'next/link'
 import styled from 'styled-components'
 
 const Footer = () => (
   <Container>
-    &copy; {new Date().getFullYear()} &middot; Meridien Consulting Services Inc.
+    <CopyRight>
+      &copy; {new Date().getFullYear()} &middot; Meridien Consulting Services
+      Inc.
+    </CopyRight>
+    <Up>
+      <Link href="#top" passHref>
+        <UpLink>↑</UpLink>
+      </Link>
+    </Up>
   </Container>
 )
 
 const Container = styled.footer`
+  display: flex;
+  align-items: center;
   padding: 2rem;
   border-top: 1px solid var(--grey-200);
-  font-size: 1.4rem;
 
   @media (min-width: ${(props) => props.theme.screen.md}) {
-    display: flex;
-    align-items: center;
     padding: 2rem 4rem;
   }
 
   @media (min-width: ${(props) => props.theme.screen.lg}) {
     padding: 2rem 8rem;
+  }
+`
+
+const CopyRight = styled.footer`
+  font-size: 1.4rem;
+`
+
+const Up = styled.footer`
+  margin-left: auto;
+`
+
+const UpLink = styled.a`
+  padding: 1rem;
+  color: var(--grey-600);
+
+  &:hover,
+  &:focus {
+    outline: none;
+    color: var(--blue-700);
+  }
+
+  @media (min-width: ${(props) => props.theme.screen.md}) {
+    padding: 0.25rem;
   }
 `
 
